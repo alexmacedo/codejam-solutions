b94169a598bc9445db34af4b692bfd08e3093d9d
Alexandre Macedo <alexbmac@gmail.com>
2012-01-02 13:09:00 -0200

    Correcting tabs in python file.

--- 2010/reverse_words.py
+++ 2010/reverse_words.py
@@ -5,9 +5,9 @@
 # url: http://code.google.com/codejam/contest/dashboard?c=351101
 
 if __name__ == "__main__":
-	n = input()
-	for i in range(1, n + 1):
-		words = raw_input().split(" ")
-		words.reverse()
-		words = " ".join(words)
-		print "Case #{0}: {1}".format(i, words)
+    n = input()
+    for i in range(1, n + 1):
+        words = raw_input().split(" ")
+        words.reverse()
+        words = " ".join(words)
+        print "Case #{0}: {1}".format(i, words)
